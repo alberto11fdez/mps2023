6c533952037e1abef4994b94276807864a826186
alberto11fdez <alberto11fdez@uma.es>
2023-02-07 11:07:38 +0100

    Add file .gitignore

---
+++ src/main/java/org/mps/ajnebro/factorial/Factorial.java
@@ -0,0 +1,18 @@
+package org.mps.ajnebro.factorial;
+
+
+public class Factorial {
+    public int compute(int value){
+        int result;
+        if(value < 0){
+            throw new NegativeValueException("This value " + value + " es negativo");
+        }else if((value == 0) || (value==1)){
+            result = 1;
+        }else {
+            result = value * compute(value-1);
+        }
+
+        return result;
+    }
+
+}
