06cb3f8cbde399a19a80f6b7079f39bec11c9beb
alberto11fdez <alberto11fdez@uma.es>
2023-02-07 16:54:02 +0100

    Ignorar

--- src/main/java/org/mps/ajnebro/factorial/Main.java
+++
@@ -1,7 +0,0 @@
-package org.mps.ajnebro.factorial;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/org/mps/ajnebro/factorial/NegativeValueException.java
+++
@@ -1,7 +0,0 @@
-package org.mps.ajnebro.factorial;
-
-public class NegativeValueException extends RuntimeException{
-    public NegativeValueException(String errorMessage) {
-        super(errorMessage);
-    }
-}
